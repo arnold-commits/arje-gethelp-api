d2fc701b63d4933c3133c47f26a5b4315602ebcc
arnold-commits <arnold@arjebookkeeping.com>
2026-05-09 20:59:09 -0700

    Initial: app/layout.js

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+    title: 'ARJE /get-help triage relay',
+    robots: { index: false, follow: false },
+}
+
+export default function RootLayout({ children }) {
+    return (
+          <html lang="en">
+            <body>{children}</body>
+      </html>
+    )
+}
